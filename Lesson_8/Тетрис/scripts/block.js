201684689b7f27dec5e6ec2e8c576ba230ddff9a
igortz <igortz@mail.ru>
2022-04-27 13:22:19 +0700

    Tetris replaced

---
+++ Lesson_8/Тетрис/scripts/block.js
@@ -0,0 +1,296 @@
+"use strict";
+
+/**
+ * Объект включает свойства и методы работы с блоками
+ */
+const block = {
+    row: 0, // текущее положение блока в игровом массиве (левый верхний угол)
+    col: 0, //
+    size: 3, // размер блока
+    type: 4, // тип блока
+    tempArray: [ // временный массив хранит блок, пока производятся вычисления
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ],
+
+    /**
+     * Возвращает ячейку текущего блока с проверкой на доступные границы
+     * @param {row: number, col: number} позиция ячейки в блоке
+     * @returns {number} Содержимое контейнера, либо 255, если row или col
+     * выходят за доступные границы
+     */
+    getCell(row, col) {
+        return container.getCell(row + this.row, col + this.col);
+    },
+
+    /**
+     * Заносит содержимое ячейки массива контейнера с проверкой на доступные границы
+     * @param {row: number, col: number} позиция ячейки в блоке
+     * @param {t: number} тип блока
+     */
+    setCell(row, col, t) {
+        container.setCell(row + this.row, col + this.col, t);
+    },
+
+    /**
+     * Чистит временный массив
+     */
+    clearTempArray() {
+        this.tempArray.forEach(element => element.fill(0));
+    },
+
+    /**
+     * Чистит текущий блок в контейнере
+     */
+    clearCurrent() {
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (this.tempArray[i][j] === this.type) {
+                    this.setCell(i, j, 0);
+                    container.drawSquare(i + this.row, j + this.col, 0);
+                }
+            }
+        }
+    },
+
+    /**
+     * Копирует текущий блок во временный массив
+     */
+    copyToTempArray() {
+        this.clearTempArray();
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if ((i + this.row) < config.rowsCount
+                    && this.getCell(i, j) === this.type) {
+                    this.tempArray[i][j] = this.getCell(i, j);
+                }
+            }
+        }
+    },
+
+    /**
+     * Возвращает блок из временного массива в игровой массив
+     */
+    getFromTempArray() {
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (this.tempArray[i][j] === this.type) {
+                    this.setCell(i, j, this.tempArray[i][j]);
+                    container.drawSquare(i + this.row, j + this.col, this.type);
+                }
+            }
+        }
+    },
+
+    /**
+     * Рисует текущий блок в контейнере
+     */
+    draw() {
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (this.getCell(i, j) === this.type) {
+                    container.drawSquare(i + this.row, j + this.col, this.type);
+                }
+            }
+        }
+    },
+
+    /**
+     * Копирует текущий блок во временный массив, удаляет текущий блок из
+     * контейнера, меняет позицию блока в контейнере, и возвращает туда блок
+     * @param {newRow: number, newCol: number} новая позиция блока в контаре
+     */
+    moveAction(newRow, newCol) {
+        this.copyToTempArray();
+        this.clearCurrent();
+        this.row = newRow;
+        this.col = newCol;
+        this.getFromTempArray();
+    },
+
+    /**
+     * Сдвигает блок вправо
+     */
+    shiftRight() {
+        let free = true;
+        // Проверяем, есть ли препятствие справа
+        for (let i = 0; i < this.size; i++) {
+            let j = 0;
+            // ищем кирпичик блока в ряду i слева направо
+            while (this.getCell(i, j) !== this.type && j < this.size) {
+                j++;
+            }
+            if (j < this.size) {
+                // ищем границу блока в ряду i
+                while (this.getCell(i, j) === this.type) {
+                    j++;
+                }
+                // если справа от блока есть препятствие (не ноль), то не free
+                if (this.getCell(i, j) !== 0) {
+                    free = false;
+                    i = this.size;
+                }
+            }
+        }
+        if (free) this.moveAction(this.row, this.col + 1);
+    },
+
+    /**
+     * Сдвигает блок влево
+     */
+    shiftLeft() {
+        let free = true;
+        // Проверяем, есть ли препятствие слева
+        for (let i = 0; i < this.size; i++) {
+            let j = this.size - 1;
+            // ищем кирпичик блока в ряду i справа налево
+            while (this.getCell(i, j) !== this.type && j > 0) {
+                j--;
+            }
+            if (j > 0) {
+                // ищем границу блока в ряду i
+                while (this.getCell(i, j) === this.type) {
+                    j--;
+                }
+                // если слева от блока есть препятствие (не ноль), то не free
+                if (this.getCell(i, j) !== 0) {
+                    free = false;
+                    i = this.size;
+                }
+            }
+        }
+        if (free) this.moveAction(this.row, this.col - 1);
+    },
+
+    /**
+     * Сдвигает блок вниз
+     * Так же генерит новый блок при достижении дна предыдущего
+     * Так же отслеживает "событие" конца игры при отсутствии
+     * свободного места для нового блока
+     * @returns {boolean} true, если блок достиг дна
+     */
+    shiftDown() {
+        // при наборе 1000 очков повышаем уровень (ускоряем падение)
+        config.level = Math.trunc(game.score / 3000);
+        if (config.level !== config.prevLevel) {
+            if (config.level < 5) config.timeInterval = 1000 - 200 * config.level;
+            clearInterval(game.timerId);
+            game.timerId = setInterval(block.shiftDown.bind(block), config.timeInterval);
+            document.querySelector('.level').textContent = 'Level : ' + config.level;
+        }
+        config.prevLevel = config.level;
+
+        let free = true;
+        let spaceEnough = true;
+        // Проверяем, есть ли препятствие снизу
+        for (let j = 0; j < this.size; j++) {
+            let i = 0;
+            // ищем кирпичик блока в столбце j сверху вниз 
+            while (this.getCell(i, j) !== this.type && i < this.size) {
+                i++;
+            }
+            if (i < this.size) {
+                // ищем границу блока в столбце j
+                while (this.getCell(i, j) === this.type) {
+                    i++;
+                }
+                // если снизу от блока есть препятствие (не ноль), то не free
+                if (this.getCell(i, j) !== 0) {
+                    free = false;
+                    j = this.size;
+                }
+            }
+        }
+        if (free) {
+            this.moveAction(this.row + 1, this.col);
+            return false;
+        } else {
+            for (let i = 0; i < this.size; i++) {
+                for (let j = 0; j < this.size; j++) {
+                    if (this.getCell(i, j) === this.type) {
+                        this.setCell(i, j, this.type + 100);
+                    }
+                }
+            }
+            container.clearBottom();
+            // проверяем, есть ли место для создания блока
+            for (let j = 0; j < nextBlock.size; j++) {
+                if (container.getCell(1,
+                    (Math.trunc(config.colsCount / 2) - nextBlock.size + 2) + j) !== 0) spaceEnough = false;
+            }
+            if (spaceEnough) {
+                this.create();
+                nextBlock.create();
+            } else {
+                // свободного места нет, Game Over
+                clearInterval(game.timerId);
+                const gameOverEl = document.querySelector('.gameOver');
+                gameOverEl.classList.remove('hide');
+                game.stop();
+                window.addEventListener('click', () => gameOverEl.classList.add('hide'));
+            }
+            return true;
+        }
+    },
+
+    /**
+     * Роняет блок на дно
+     */
+    dropDown() {
+        while (!this.shiftDown()) { }
+    },
+
+    /**
+     * Вращает блок по часовой стрелки
+     */
+    rotate() {
+        let free = true;
+        if ((this.row + this.size - 1) < config.rowsCount) {
+            if (this.col < 0) this.shiftRight();
+            if (this.col > config.colsCount - this.size) this.shiftLeft();
+
+            // проверяем, есть ли место для разворота
+            for (let i = 0; i < this.size; i++) {
+                if (this.getCell(i, 0) !== 0 && this.getCell(i, 0) !== this.type)
+                    free = false;
+                if (this.getCell(i, this.size - 1) !== 0 && this.getCell(i, this.size - 1) !== this.type)
+                    free = false;
+            }
+
+            if (free) {
+                this.copyToTempArray();
+                this.clearCurrent();
+                // Копируем блок из временного массива с разворотом
+                for (let i = 0; i < this.size; i++) {
+                    for (let j = 0; j < this.size; j++) {
+                        container.setCell(j + this.row,
+                            this.size - i - 1 + this.col, this.tempArray[i][j]);
+                    }
+                }
+                this.draw();
+            }
+
+        }
+    },
+
+    /**
+     * Метод создает новый блок в игровом массиве типа type.
+     */
+    create() {
+        this.col = Math.trunc(config.colsCount / 2) - this.size + 2;
+        this.row = 0;
+        this.type = nextBlock.type;
+        this.size = nextBlock.size;
+        for (let i = 0; i < this.size; i++) {
+            for (let j = 0; j < this.size; j++) {
+                if (nextBlock.array[i][j] === this.type) {
+                    this.setCell(i, j, nextBlock.array[i][j]); // [row][col]
+                }
+            }
+        }
+        this.draw();
+    },
+
+};
---
+++ Lesson_8/Тетрис/scripts/game.js
@@ -0,0 +1,158 @@
+"use strict";
+
+/**
+ * Объект включает свойства и методы игрового интерфейса (ui)
+ */
+const game = {
+    score: 0,  // очки за игру
+    timerId: 0,
+    active: false,
+
+    /**
+     * Запускает новую игру
+     */
+    new() {
+        container.draw();
+        container.clear();
+        this.score = 0;
+        nextBlock.clear();
+        nextBlock.create();
+        block.create();
+        this.score = 0;
+        config.level = 0;
+        config.prevLevel = 0;
+        config.timeInterval = 1000;
+        // Инициализируем экран новой игры. Прячем, что не недо, и
+        // показываем то, что надо
+        document.querySelector('.score').textContent = 'Score : 0';
+        document.querySelector('.level').textContent = 'Level : 0';
+        document.querySelector('.sizeSelector').classList.add('hide');
+        document.querySelector('.nextBlock').classList.remove('hide');
+        document.querySelector('.info').classList.remove('hide');
+        document.querySelector('.lowerNav').classList.remove('hide');
+        // Запускаем метод падения блока раз в timeInterval (1 сек)
+        game.timerId = setInterval(block.shiftDown.bind(block), config.timeInterval);
+        newBtnEl.textContent = 'New Game';
+        game.active = true;
+    },
+
+    stop() {
+        clearInterval(game.timerId);
+        document.querySelector('.info').classList.add('hide');
+        document.querySelector('.lowerNav').classList.add('hide');
+        document.querySelector('.sizeSelector').classList.remove('hide');
+        document.querySelector('.nextBlock').classList.add('hide');
+        document.querySelector('table').remove();
+        newBtnEl.textContent = 'Start';
+        game.active = false;
+    }
+};
+
+// Инициация при открытии страницы с игрой
+
+nextBlock.draw();
+nextBlock.clear();
+
+const btnPauseEl = document.querySelector('.btnPause');
+const btnGridEl = document.querySelector('.btnGrid');
+const pauseEl = document.querySelector('.pause');
+const tableEl = document.querySelector('tab');
+const largeBtn = document.querySelector('.large');
+const mediumBtn = document.querySelector('.medium');
+const smallBtn = document.querySelector('.small');
+const newBtnEl = document.querySelector('.btnNewGame');
+
+// обработка нажатий клавиш управления
+window.addEventListener('keydown', (event) => action.move(event));
+
+// запускает новую игру при нажатии кнопки "New Game"
+newBtnEl.addEventListener('click', () => {
+    if (game.active) game.stop();
+    else game.new();
+});
+
+// обрабатывает нажатие кнопки темы (светлая/тёмная)
+btnGridEl.addEventListener('click', () => {
+    if (btnGridEl.classList.contains('tableDark')) {
+        btnGridEl.classList.remove('tableDark');
+        config.theme = 'blockLight';
+    } else {
+        btnGridEl.classList.add('tableDark');
+        config.theme = 'blockDark';
+    };
+    container.drawArray();
+});
+
+// обрабатывает нажатие кнопки "Pause"
+btnPauseEl.addEventListener('click', event => {
+    clearInterval(game.timerId);
+    pauseEl.classList.remove('hide');
+    event.stopPropagation();
+    window.addEventListener('click', () => {
+        if (!pauseEl.classList.contains('hide')) {
+            pauseEl.classList.add('hide');
+            game.timerId = setInterval(block.shiftDown.bind(block), config.timeInterval);
+        }
+    });
+});
+
+// обработка кнопок управления движением блока
+document.getElementById('arrowUp').addEventListener('click', () => block.rotate());
+document.getElementById('arrowDown').addEventListener('click', () => {
+    block.shiftDown();
+    game.score++;
+    document.querySelector('.score').textContent = 'Score : ' + game.score;
+});
+document.getElementById('arrowLeft').addEventListener('click', () => block.shiftLeft());
+document.getElementById('arrowRight').addEventListener('click', () => block.shiftRight());
+document.getElementById('space').addEventListener('click', () => {
+    game.score += config.rowsCount - block.row;
+    block.dropDown();
+    document.querySelector('.score').textContent = 'Score : ' + game.score;
+});
+
+// обработка другимих элементов управления 
+document.querySelector('nav').addEventListener('click', event => {
+
+    // размер контейнера для игры
+    if (event.target.classList.contains('bordSize')) {
+        document.querySelectorAll('.bordSize').
+            forEach(item => item.classList.remove('sizeSelected'));
+        event.target.classList.toggle('sizeSelected');
+        switch (event.target.dataset.size) {
+            case '20x10':
+                config.rowsCount = 20;
+                config.colsCount = 10;
+                break;
+            case '25x15':
+                config.rowsCount = 25;
+                config.colsCount = 15;
+                break;
+            case '30x20':
+                config.rowsCount = 30;
+                config.colsCount = 20;
+                break;
+        }
+    }
+    // размер ячеек в контейнере
+    largeBtn.classList.remove('checkedSize');
+    mediumBtn.classList.remove('checkedSize');
+    smallBtn.classList.remove('checkedSize');
+    if (event.target.classList.contains('large') ||
+        event.target.classList.contains('largeImg')) {
+        config.cellSize = "tdSizeL";
+    } else if (event.target.classList.contains('medium') ||
+        event.target.classList.contains('mediumImg')) {
+        config.cellSize = "tdSizeM";
+    } else if (event.target.classList.contains('small') ||
+        event.target.classList.contains('smallImg')) {
+        config.cellSize = "tdSizeS";
+    }
+    if (config.cellSize === "tdSizeL") largeBtn.classList.add('checkedSize');
+    if (config.cellSize === "tdSizeM") mediumBtn.classList.add('checkedSize');
+    if (config.cellSize === "tdSizeS") smallBtn.classList.add('checkedSize');
+
+    // после изменений перерисовываем игровой контейнер
+    if (game.active) container.drawArray();
+    nextBlock.drawArray();
+});
